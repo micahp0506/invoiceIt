642f8829d223631c547ddc721c8a119d7159b35e
Micah Wells <micahp0506@gmail.com>
2016-03-06 12:05:42 -0600

    Adding angular routes and controllers

--- client/app.js
+++ client/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+const myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(($routeProvider) => {
+    $routeProvider
+        .when('/', {
+            controller: 'DashboardController',
+            templateURL: 'views/dashboard.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
---
+++ client/controllers/dashboard.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+myApp.controller('DashboardController', ['$scope', '$http', '$location',($scope, $http, $location) => {
+    console.log("Dashboard Controller loaded.....");
+}]);

f88d16e3b55158ddaedd2d2cf97d6c634be08d72
Micah Wells <micahp0506@gmail.com>
2016-03-10 14:33:46 -0600

    Added connection db through mlab for deployment on heroku

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ const mongoose = require('mongoose');
 const customers = require('./routes/customers');
 const invoices = require('./routes/invoices');
 
-// Mongoose connection
-mongoose.connect('mongodb://localhost/invoiceit');
-const db = mongoose.connection;
+
 // Defining PORT, 3000 or process.env
 const PORT = process.env.PORT || 3000;
+
+// MongoDB and mLab connection
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_NAME = process.env.MONGODB_NAME || 'invoiceit';
+
+const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
+
+const MONGODB_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`;
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
@@ -26,10 +35,14 @@ app.get('/', (req, res) => {
 app.use('/api/customers', customers);
 app.use('/api/invoices', invoices);
 
-// Server listening
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+// Mongoose connection
+mongoose.connect(MONGODB_URL);
+mongoose.connection.on('open', () => {
+  console.log('MONGO OPEN');
+  app.listen(PORT, () => {
+  console.log(`Node.js server has started. Listening on port ${PORT}`);
+  });
+})
 
 
 module.exports = app;

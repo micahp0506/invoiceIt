61a298ed187d68e48afa9e3c22667781475dc2f2
Micah Wells <micahp0506@gmail.com>
2016-03-10 14:50:54 -0600

    Getting mlab connection working

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
 const MONGODB_USER = process.env.MONGODB_USER || '';
 const MONGODB_PASS = process.env.MONGODB_PASS || '';
-const MONGODB_NAME = process.env.MONGODB_NAME || 'invoiceit';
+
 
 const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
 
-const MONGODB_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`;
+const MONGODB_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/invoiceit`;
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
